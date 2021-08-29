e1c0002802df125e3ac10805e82968e00933190e
Ryan S <wuuug@telegram>
2021-08-29 03:40:01 -0700

    Add a few more types

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
 type command string
+type trailer interface {
+	command() string
+}
+
+type break_trailer struct {}
+func (t break_trailer) command() string { return "break" }
+
+type exec_trailer struct {
+	cmd string
+}
+func (t exec_trailer) command() string { return fmt.Sprintf("exec %s", t.cmd) }
+
+type reaction struct {
+	mode command
+	auxiliary []trailer
+}
 
 var commands = map[string]command{
 	"pick":   "pick",
