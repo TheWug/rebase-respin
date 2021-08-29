a079be9a9639eb3fe6fc2a3cc00f9411df20251e
Ryan S <wuuug@telegram>
2021-08-29 14:39:45 -0700

    Add tests for output_node functions

--- main_test.go
+++ main_test.go
@@ -9,9 +9,32 @@ func Test_trailers(t *testing.T) {
 	if msg != "break" {
 		t.Errorf("Unexpected value for break_trailer.command(): got %s, expected break", msg)
 	}
-	
+
 	msg = exec_trailer{cmd: "ls -l"}.command()
 	if msg != "exec ls -l" {
 		t.Errorf("Unexpected value for break_trailer.command(): got '%s', expected 'exec ls -l'", msg)
+	}
+}
+
+func Test_output_node(t *testing.T) {
+	var f func()
+	defer func(){
+		r := recover()
+		if r != nil { f() }
+	}()
+
+	head, tail := newList()
+	f = func() { t.Errorf("linked list creator helper is not properly initializing its contents! %p: %+v, %p: %+v", head, head, tail, tail) }
+
+	if head.next.prev.next != tail {
+		t.Errorf("linked list creator helper is not properly initializing its contents! %p: %+v, %p: %+v", head, head, tail, tail)
+	}
+
+	node := &output_node{}
+	head.insert_after(node)
+	f = func() { t.Errorf("node insertion is behaving strangely! %p: %+v, %p: %+v, %p: %+v", head, head, node, node, tail, tail) }
+
+	if head.next.next.prev.prev.next.next != tail {
+		t.Errorf("node insertion is behaving strangely! %p: %+v, %p: %+v, %p: %+v", head, head, node, node, tail, tail)
 	}
 }
