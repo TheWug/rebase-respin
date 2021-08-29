b944c354230df5ca19ceae9b0d109a3955e2c073
Ryan S <wuuug@telegram>
2021-08-29 03:50:48 -0700

    Add command type alias and map of rebase commands for consistenct's sake

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
 package main
 
+type command string
+
+var commands = map[string]command{
+	"pick":   "pick",
+	"p":      "pick",
+	"reword": "reword",
+	"r":      "reword",
+	"edit":   "edit",
+	"e":      "edit",
+	"squash": "squash",
+	"s":      "squash",
+	"fixup":  "fixup",
+	"f":      "fixup",
+	"drop":   "drop",
+	"d":      "drop",
+	"exec":   "exec",
+	"x":      "exec",
+	"break":  "break",
+	"b":      "break",
+	"override": "",
+	"o":        "",
+	"label":    "",
+	"l":        "",
+	"reset":    "",
+	"t":        "",
+	"merge":    "",
+	"m":        "",
+	"":         "",
+}
+
 func main() {
 }
