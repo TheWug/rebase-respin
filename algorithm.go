bd05e63c2533281fad1239477cd4e3b723a24f34
Ryan S <wuuug@telegram>
2021-08-29 12:58:20 -0700

    Migrate inline helpers out of main for ongoing algorithm decoupling

--- algorithm.go
+++ algorithm.go
@@ -5,6 +5,39 @@ import (
 	"strings"
 )
 
+func push(s string, head *output_node) {
+	head.insert_after(&output_node{line: s})
+}
+
+func push_commit(s, msg string, t []trailer, head *output_node, commits_by_message map[string]*output_node) {
+	node := &output_node{line: s, msg: msg, trailers: t}
+	commits_by_message[msg] = node
+	head.insert_after(node)
+}
+
+func relocate_commit(s, msg string, t []trailer, head *output_node, commits_by_message map[string]*output_node) error {
+	node := &output_node{line: s, msg: msg, trailers: t}
+	commits_by_message[msg] = node
+	for {
+		token, new_msg := grab(msg)
+		if token != "fixup!" && token != "squash!" {
+			return fmt.Errorf("Couldn't figure out where to place commit: %s", s)
+		}
+
+		old_node, ok := commits_by_message[new_msg]
+		if ok {
+			for strings.Contains(old_node.msg, new_msg) && old_node.prev != nil {
+				old_node = old_node.prev
+			}
+			old_node.insert_after(node)
+			return nil
+		}
+
+		msg = new_msg
+	}
+	panic("not reached")
+}
+
 // typical implementer is bufio.Scanner
 type myscanner interface {
 	Scan() bool
